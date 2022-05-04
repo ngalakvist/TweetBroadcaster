cac9e955cf0a912e9e8204850c4a0e2294b699c4
talng <talng@98-36002>
2022-05-04 09:57:18 +0200

    Add project files.

---
+++ TweetBroadcaster/Config/SwaggerConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace TweetBroadcaster.Config
+{
+    public class SwaggerConfig
+    {
+        public string JsonRoute { get; set; }
+        public string Description { get; set; }
+        public string UIEndPoint { get; set; }
+            
+    }
+}
---
+++ TweetBroadcaster/Startup.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TweetBroadcaster.Config;
+using Microsoft.OpenApi.Models;
+
+namespace TweetBroadcaster
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<Data.DataContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<Data.DataContext>();
+            services.AddControllersWithViews();
+            services.AddMvc();
+            services.AddSwaggerGen(services =>
+            {
+                services.SwaggerDoc("v1", new OpenApiInfo { Title = "TweetBroadcast Api", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            var swaggerConfig = new SwaggerConfig();
+            Configuration.GetSection(nameof(SwaggerConfig)).Bind(swaggerConfig);
+
+            app.UseSwagger(config =>
+            {
+                config.RouteTemplate = swaggerConfig.JsonRoute;
+            });
+            app.UseSwaggerUI(config =>
+            {
+                config.SwaggerEndpoint(swaggerConfig.UIEndPoint,swaggerConfig.Description);
+            });
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
